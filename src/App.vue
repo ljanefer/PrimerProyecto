f273c9727a05a49ca50781cf2b89a1382a8b092c
Laura <ljanefer@gmail.com>
2022-07-06 23:32:45 -0300

    primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,76 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <div class="row">
+      <div class="container">
+        <div class="row">
+          <div class="col" v-for="(producto, index) in productos" :key="index">
+            <CardProducto :producto="producto" />
+          </div>
+        </div>
+      </div>
+    </div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import CardProducto from './components/CardProducto.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Header, CardProducto
+  },
+  data() {
+    return {
+      productos: [
+        {
+          nombre: "Caramelera",
+          precio: "$1000",
+          imagen: "caramelera.jpg",
+          descripcion: "Esta es una descripcion de una vela con envase",
+        },
+        {
+          nombre: "Vela envase con tapa metal",
+          precio: "$1500",
+          imagen: "unavela.jpg",
+          descripcion: "Esta es una descripcion de una vela con envase",
+        },
+        {
+          nombre: "Difusor",
+          precio: "$1400",
+          imagen: "difusor.jpg",
+          descripcion: "Esta es una descripcion de una vela con envase",
+        },
+        {
+          nombre: "Esferas aromáticas",
+          precio: "$500",
+          imagen: "bolitas.jpg",
+          descripcion: "Esta es una descripcion de una vela con envase",
+        },
+        {
+          nombre: "carrusel",
+          precio: "$1200",
+          imagen: "carrusel.jpg",
+          descripcion: "Esta es una descripcion de una vela con envase",
+        },
+        {
+          nombre: "envase rojo",
+          precio: "$1500",
+          imagen: "velaroja.jpg",
+          descripcion: "Esta es una descripcion de una vela con envase",
+        },
+      ],
+    };
+  },
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Montserrat, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
